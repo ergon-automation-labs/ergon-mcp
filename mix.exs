3fa3fe471fddc53448be6be30837b143b89d4c61
Abby Malson <august.malson@gmail.com>
2026-09-20 11:45:22 -0600

    chore(release): adopt bot_army_library_runtime 0.14.85 (health monitor: alerts are never heartbeats) (v0.2.18)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyMcp.MixProject do
   def project do
     [
       app: :bot_army_mcp,
-      version: "0.2.17",
+      version: "0.2.18",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
